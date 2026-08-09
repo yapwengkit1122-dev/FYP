4942fabcff02e9c6e3da3395dd1cf253f2647476
yapwengkit1122-dev <yapwengkit1122@gmail.com>
2026-08-09 20:46:00 +0800

    Update api_attendance.php

--- api_attendance.php
+++ api_attendance.php
@@ -1,7 +1,7 @@
 <?php
 header('Content-Type: application/json');
 
-$conn = new mysqli('localhost', 'root', 'pwd123', 'attendance_system');
+$conn = new mysqli('localhost', '', '', 'attendance_system');
 
 if ($conn->connect_error) {
     echo json_encode([]);
